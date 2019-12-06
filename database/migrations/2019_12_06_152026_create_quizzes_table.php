8653ceb69f2b622206b9c3920fb2340d133ccea8
Brad Stapleton <brad@rethinkcanada.com>
2019-12-06 14:49:29 -0500

    add some more files

---
+++ database/migrations/2019_12_06_152026_create_quizzes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuizzesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->text('email');
+            $table->string('question_1');
+            $table->string('question_2');
+            $table->string('question_3');
+            $table->string('question_4');
+            $table->string('question_5');
+            $table->string('question_6');
+            $table->string('question_7');
+            $table->string('question_8');
+            $table->string('question_9');
+            $table->string('question_10');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quizzes');
+    }
+}
